5431b3ff3959e9f116b414ef954e61002d249f97
Raphael <90244512+Rapehl@users.noreply.github.com>
2021-09-07 16:33:07 +0200

    Look Direction
    J'ai fait en sorte qu'on puisse regarder où l'on veut avec le stick droit de la manette

--- Assets/Raphael/Scripts/PlayerController.cs
+++ Assets/Raphael/Scripts/PlayerController.cs
@@ -30,9 +30,27 @@ public class PlayerController : MonoBehaviour
         Vector3 movement = new Vector3(moveX, 0, moveZ);
         rb.velocity = movement.normalized * speed;
 
-        if (movement != Vector3.zero)
+        float lookX = player.GetAxis("Look X");
+        float lookZ = player.GetAxis("Look Z");
+
+        Vector3 lookDirection = new Vector3(lookX, 0, lookZ);
+        lookDirection = lookDirection.normalized;
+
+        if (movement != Vector3.zero && lookDirection == Vector3.zero)
         {
             UnityEngine.Quaternion toRotation = UnityEngine.Quaternion.LookRotation(movement, Vector3.up);
+            transform.rotation = UnityEngine.Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+
+        else if (movement == Vector3.zero && lookDirection != Vector3.zero)
+        {
+            UnityEngine.Quaternion toRotation = UnityEngine.Quaternion.LookRotation(lookDirection, Vector3.up);
+            transform.rotation = UnityEngine.Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+
+        else if (movement != Vector3.zero && lookDirection != Vector3.zero)
+        {
+            UnityEngine.Quaternion toRotation = UnityEngine.Quaternion.LookRotation(lookDirection, Vector3.up);
             transform.rotation = UnityEngine.Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
         }
     }
