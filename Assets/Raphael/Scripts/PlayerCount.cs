a99998cbe6f7fdd5bcfef1d32defcb0db8b21725
Raphael <90244512+Rapehl@users.noreply.github.com>
2021-09-08 21:44:37 +0200

    Mesh différents pour les joueurs !
    Chaque nouveau joueur qui rejoint aura un mesh différent.

--- Assets/Raphael/Scripts/PlayerCount.cs
+++ Assets/Raphael/Scripts/PlayerCount.cs
@@ -1,15 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json.Bson;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerCount : MonoBehaviour
 {
+    private PlayerInputManager inputManager;
+    public GameObject playerPrefabB;
+    public GameObject playerPrefabC;
+    public GameObject playerPrefabD;
+
     [HideInInspector]
     public int playerCount = 0;
+
+    void Start()
+    {
+        inputManager = gameObject.GetComponent<PlayerInputManager>();
+    }
+
     public void AddPlayer()
     {
         playerCount++;
         Debug.Log("Player " + playerCount + " joined the game.");
+
+        if (playerCount == 1)
+        {
+            inputManager.playerPrefab = playerPrefabB;
+        }
+
+        else if (playerCount == 2)
+        {
+            inputManager.playerPrefab = playerPrefabC;
+        }
+
+        else if (playerCount == 3)
+        {
+            inputManager.playerPrefab = playerPrefabD;
+        }
     }
 
 }
