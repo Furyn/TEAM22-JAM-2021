03e0dc8175e6e95d73ba1a53dff27147ee0aa016
Vincent <vincent_1999@hotmail.fr>
2021-09-09 11:39:47 +0200

    Add event IA

--- Assets/Vincent/Script/Event.cs
+++ Assets/Vincent/Script/Event.cs
@@ -3,30 +3,46 @@ using UnityEngine;
 public class Event : MonoBehaviour
 {
     [SerializeField] private string Name = "Event Name";
-    public bool eventLaunch = false;
     [SerializeField] private float timerDuration = 10f;
+    internal Transform startTerainPosition = null;
+    internal Transform endTerainPosition = null;
+
+    public bool eventLaunch = false;
     private float timer = 0f;
+
+    public virtual void SetUpPostion(Transform start, Transform end)
+    {
+        startTerainPosition = start;
+        endTerainPosition = end;
+    }
 
     public virtual void Apply()
     {
         eventLaunch = true;
         timer = timerDuration;
     }
+
     public virtual void Stop()
     {
         eventLaunch = false;
         timer = 0f;
     }
 
-    private void Update()
+    public virtual void UpdateEvent()
     {
         if (eventLaunch)
         {
+            Debug.Log(timer);
             timer -= Time.deltaTime;
             if (timer <= 0f)
             {
                 Stop();
             }
         }
+    }
+
+    private void Update()
+    {
+        UpdateEvent();
     }
 }
